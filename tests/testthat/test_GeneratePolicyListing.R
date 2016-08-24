d63dd9a985bb35c2e41b07cd6564610430310d6d
actuarialvoodoo <rthomas2003@gmail.com>
2016-08-25 10:45:14 +1200

    Added new tests for generatePolicyList

--- tests/testthat/test_GeneratePolicyListing.R
+++ tests/testthat/test_GeneratePolicyListing.R
@@ -8,13 +8,18 @@ endDtOneYear <- startDt + lubridate::years(1) - lubridate::days(1)
 total_rows <- function(grates){
      sum(vapply(cumprod(grates), function(x) ceiling(x * base), FUN.VALUE = numeric(1)))
 }
-num_rows <- function(years, ...){
+num_rows <- function(base, years, ...){
      endDt <- startDt + lubridate::years(years) - lubridate::days(1)
      nrow(generatePolicyList(base, startDt, endDt, ...))
 }
 
 test_that("Complete Calendar Years Equal Total", {
-     expect_that(num_rows(1), equals(base))
-     expect_that(num_rows(3), equals(3 * base))
-     expect_that(num_rows(3, growthRate = 0.05), equals(total_rows(c(1, 1.05, 1.05))))
+     expect_that(num_rows(base, 1), equals(base))
+     expect_that(num_rows(base, 3), equals(3 * base))
+     expect_that(num_rows(base, 3, growthRate = 0.05), equals(total_rows(c(1, 1.05, 1.05))))
+     expect_that(num_rows(base, 5, growthRate = c(0.05, 0.06, 0.07)), equals(total_rows(c(1, 1.05, 1.06, 1.07, 1.07))))
+     expect_that(num_rows(c(1000, 1100, 1500), 3), equals(3600))
+     expect_that(num_rows(c(1000, 1100, 1500), 4), equals(5100))
+     expect_that(num_rows(c(1000, 1100, 1500), 2), equals(2100))
+     expect_that(num_rows(c(1000, 1100, 1500), 5, growthRate = c(0.05, 0.06)), equals(6845))
 })
